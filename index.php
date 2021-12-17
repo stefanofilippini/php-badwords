e95e60ff82c2b4056d3134e568ac6aebd2510f97
stefanofilippini <filippini.stefano96@gmail.com>
2021-12-17 15:31:06 +0100

    done

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test php</title>
+</head>
+<body>
+    <?php
+        $censure = $_GET['word'];
+        $testo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nesciunt nam consequatur laboriosam delectus exercitationem blanditiis mollitia autem voluptatibus? Dolorem obcaecati ea quo est repudiandae temporibus? Quo cum quas fugiat.';
+        echo $censure;
+        echo '<br>';
+        echo str_replace($censure, '***', $testo );
+    ?>
+</body>
+</html>
